c98172ea826cf9259979c7ee44113f9699499f45
X99ror <144711429+X99ror@users.noreply.github.com>
2025-06-17 19:29:15 +0530

    data transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,8 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation 
+from src.components.data_transformation import DataTransformationConfig
 
 @dataclass
 class DataingestionConfig:
@@ -47,5 +49,8 @@ class Dataingestion:
         
 if __name__=="__main__":
     obj=Dataingestion()
-    obj.initiate_data_ingestion()       
+    train_data,test_data=obj.initiate_data_ingestion()
+    data_transform=DataTransformation()
+    data_transform.initiate_data_transformation(train_data,test_data)
+           
             
--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -1,1 +1,101 @@
 ## all code related to transformation of data 
+import sys
+import os
+from src.logger import logging
+from dataclasses import dataclass
+from src.utils import save_object
+import numpy as np 
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder,StandardScaler
+
+from src.exception import CustomException
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj_file_path=os.path.join("artifacts","preprocessor.pkl")
+    
+class DataTransformation:
+    def __init__(self):
+        self.data_transform_config=DataTransformationConfig()
+    
+    def get_transformer_object(self):
+          try:
+              numerical_columns=['reading_score', 'writing_score']
+              categorical_columns=['gender', 'race_ethnicity', 'parental_level_of_education', 'lunch', 'test_preparation_course']
+              num_pipeline=Pipeline(
+                  steps=[
+                      ("imputer",SimpleImputer(strategy="median")),
+                      ("scaler",StandardScaler())
+                  ]
+              ) 
+              cat_pipeline=Pipeline(
+                  steps=[
+                      ("imputer",SimpleImputer(strategy="most_frequent")),
+                      ("encoder",OneHotEncoder()),
+                      ("scaler",StandardScaler(with_mean=False))
+                  ]
+              ) 
+              
+              logging.info("Numerical columns Standardization done!")
+              logging.info("Categorical columns encoding done!")
+              
+              preprocessor=ColumnTransformer(
+                  [
+                      ("Numerical pipeline",num_pipeline,numerical_columns),
+                      ("Categorical pipeline",cat_pipeline,categorical_columns)
+                  ]
+              )
+              return preprocessor
+              
+          except Exception as e:
+              raise CustomException
+    
+    def initiate_data_transformation(self,train_path,test_path):
+        try:
+            train_df=pd.read_csv(train_path)
+            test_df=pd.read_csv(test_path)
+            
+            logging.info("Read train test data completed")
+            logging.info("Obtaining preprocessing object")
+            
+            preprocessing_obj=self.get_transformer_object()
+            target_column="math_score"
+            numerical_columns=['reading_score', 'writing_score']
+            
+            input_feature_train_df=train_df.drop(columns=[target_column],axis=1)
+            target_feature_train_df=train_df[target_column]
+            
+            input_feature_test_df=test_df.drop(columns=[target_column],axis=1)
+            target_feature_test_df=test_df[target_column]
+            
+            logging.info(
+                f"Applying preprocessing object on training dataframe and testing data frame"
+            )  
+            input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)     
+            # np.c_ is a NumPy shorthand used to concatenate arrays column-wise
+            
+            train_arr=np.c_[
+                input_feature_train_arr,np.array(target_feature_train_df)
+            ]
+            
+            test_arr=np.c_[
+                input_feature_test_arr,np.array(target_feature_test_df)
+            ]
+            
+            save_object(
+                file_path=self.data_transform_config.preprocessor_obj_file_path,
+                obj=preprocessing_obj
+            )
+            return(
+                train_arr,
+                test_arr,
+                self.data_transform_config.preprocessor_obj_file_path,
+                
+            )
+                
+        except Exception as e:
+            raise CustomException(e,sys)     
